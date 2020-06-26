45b70a09e2a87cf75e775d688379e18f918adc90
Tareq Mia <tareqmia81@gmail.com>
2020-06-25 22:09:31 -0400

    Updates

---
+++ precache-manifest.89770e7df3f6270e0a3bda379ee478aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16562194c3e1c2d24b8c868dee2e9bef",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "ad1299d33844c4c671da",
+    "url": "/Pomodoro-Clock/static/css/main.9b505b56.chunk.css"
+  },
+  {
+    "revision": "e0727c998efb6d615e2b",
+    "url": "/Pomodoro-Clock/static/js/2.05a37680.chunk.js"
+  },
+  {
+    "revision": "5d7719635e368d1c17de69a19973b100",
+    "url": "/Pomodoro-Clock/static/js/2.05a37680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1299d33844c4c671da",
+    "url": "/Pomodoro-Clock/static/js/main.022029ce.chunk.js"
+  },
+  {
+    "revision": "f6bf13564b4c95a28f66",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.f27f4606.js"
+  }
+]);
